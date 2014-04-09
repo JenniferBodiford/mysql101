a53823723f9e5c2eb1e32e9a9f8e5bb4e9081d53
JenniferBodiford <jbodiford@boomtownroi.com>
2014-04-09 19:46:13 -0400

    Exercise 3
    Select statements

--- SQLScripts/Day2HomeworkBodiford/Select.sql
+++ SQLScripts/Day2HomeworkBodiford/Select.sql
@@ -1,4 +1,16 @@
 USE rockstarday2;
 show tables;
 
-SELECT ID, Name, Genre from Band;
+#SELECT * FROM Individual
+#WHERE LastName='Jennings'
+
+#SELECT ID, LastName
+#FROM Individual
+#WHERE DeceasedDate iS NOT NULL;
+
+#SELECT ID,LastName,BirthDate
+#FROM Individual
+#WHERE Year(BirthDate) > 1940;
+
+SELECT * FROM Individual
+WHERE ID in (1,3,5,7,19);
